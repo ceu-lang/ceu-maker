0aaf95baa34edfc9553e47d2c00c16f15122d92f
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-07-26 23:17:02 -0300

    (>) small fixes, directories

--- ceu-maker/run/make-arduino.bat
+++ ceu-maker/run/make-arduino.bat
@@ -15,13 +15,13 @@ set PATH=%~dp0\..\mingw\bin
 
 cd %~dp0
 
-lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input="%CEU_SRC%" --ceu --ceu-err-unused=pass --ceu-err-uninitialized=pass --ceu-features-lua=false --ceu-features-thread=false --ceu-features-isr=%CEU_ISR% --env --env-types=ceu\env\arduino\types.h --env-output=ceu\env\arduino\_ceu_app.c.h
+lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input="%CEU_SRC%" --ceu --ceu-err-unused=pass --ceu-err-uninitialized=pass --ceu-features-lua=false --ceu-features-thread=false --ceu-features-isr=%CEU_ISR% --env --env-types=ceu\env\arduino\env\types.h --env-output=ceu\env\arduino\env\_ceu_app.c.h
 if ERRORLEVEL 1 goto ERROR
 
-..\arduino-1.8.3\arduino.exe --board arduino:%ARD_ARCH%:%ARD_BOARD%%ARD_CPU% --port %ARD_PORT% --upload ceu\env\arduino\env.ino
+..\arduino-1.8.3\arduino.exe --board arduino:%ARD_ARCH%:%ARD_BOARD%%ARD_CPU% --port %ARD_PORT% --upload ceu\env\arduino\env\env.ino
 if ERRORLEVEL 1 goto ERROR
 
-exit
+rem exit
 
 :ERROR
 pause
