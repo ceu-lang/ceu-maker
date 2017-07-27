b828abd7ed3aa37b624f567d6c12e36af8c352e0
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-07-27 11:00:50 -0300

    (>) small fixes

--- ceu-maker/run/make-pico.bat
+++ ceu-maker/run/make-pico.bat
@@ -6,14 +6,14 @@ for %%A in ("%CEU_SRC%") do (
     set CEU_DIR=%%~dpA
 )
 
-mkdir %CEU_DIR%\dist\ > NUL
-mkdir %CEU_DIR%\dist\res\ > NUL
-copy dist\*.* %CEU_DIR%\dist\ > NUL
-copy %CEU_DIR%\res\*.* %CEU_DIR%\dist\res\ > NUL
-copy res\*.* %CEU_DIR%\dist\res\ > NUL
-
 set PATH=%~dp0\..\mingw\bin
 cd %~dp0
+
+mkdir %CEU_DIR%\dist\ 2>NUL
+mkdir %CEU_DIR%\dist\res\ 2>NUL
+copy dist\*.* %CEU_DIR%\dist\ >NUL
+copy %CEU_DIR%\res\*.* %CEU_DIR%\dist\res\ >NUL
+copy res\*.* %CEU_DIR%\dist\res\ >NUL
 
 lua53.exe ceu.lua --pre --pre-args="-Iceu\include -DCEU_SRC=%CEU_SRC%" --pre-input="ceu\pico.ceu" --ceu --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_gfx -lSDL2_image -lSDL2_mixer -lSDL2_ttf -lSDL2_net" --cc-output=%CEU_DIR%\dist\tmp.exe
 if ERRORLEVEL 1 goto ERROR
