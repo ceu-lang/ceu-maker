c378bfb40e4bb5230aebec96cfa287f321d7e913
Anny Caroline <annycarolinegnr@gmail.com>
2018-07-09 11:08:01 -0300

    Fixes #12, Related #13

--- ceu-maker/run/make-arduino.bat
+++ ceu-maker/run/make-arduino.bat
@@ -1,4 +1,20 @@
 rem @echo off
 cd %~dp0\..\repos\ceu-arduino\
-..\..\mingw\bin\make -f Makefile CEU_SRC=%1
-pause
+
+rem https://stackoverflow.com/questions/11153532/how-to-iterate-string-in-cmd-for-loop
+for /f "tokens=1-3" %%a in ('%~dp0SelectDevice.exe') do (
+
+    if NOT "%%a" == "" ( 
+        rem the user didn't close the SelectDevice.exe using the 'X' from the window
+
+        if "%%a" == "ide" (
+            rem the user choosed to use the IDE configurations
+            ..\..\mingw\bin\make -f Makefile CEU_SRC=%1 IDE=t
+        ) else (
+            rem the user choosed a board from the list
+            ..\..\mingw\bin\make -f Makefile CEU_SRC=%1 ARD_BOARD=%%a ARD_PORT=%%b ARD_CPU=%%c
+        )
+
+        pause
+    )
+)
