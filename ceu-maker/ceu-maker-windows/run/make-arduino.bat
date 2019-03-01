8f48754948493d33c2b3f9af5d5a905a489c81c6
AnnyCaroline <annycarolinegnr@gmail.com>
2019-03-01 18:41:11 -0300

    [windows] Céu-Arduino - store board info at board.conf file

--- ceu-maker/ceu-maker-windows/run/make-arduino.bat
+++ ceu-maker/ceu-maker-windows/run/make-arduino.bat
@@ -1,19 +1,40 @@
-rem @echo off
+@echo off
+
+for %%i IN ("%1") do (set fileextension=%%~xi)
+
+rem if we're trying to compile a folder
+if "%fileextension%"=="" (
+    set boardPath=%1\board.conf
+) else (
+    set boardPath=board.conf
+)
 
 rem https://stackoverflow.com/questions/206114/batch-files-how-to-read-a-file
-for /f "tokens=1-4" %%a in (board.conf) do (
-    rem try to use the conf file
+for /f "tokens=1-4" %%a in (%boardPath%) do (
+    rem try to use the project conf file
     set e=%%a
     set f=%%b
     set g=%%c
     set h=%%d
 )
 
-rem if the conf file is empty
+rem if the project conf file is empty
+if "%e%" == "" ( 
+    for /f "tokens=1-4" %%a in (%~dp0board.conf) do (
+        rem try to use the global conf file
+        set e=%%a
+        set f=%%b
+        set g=%%c
+        set h=%%d
+    )
+)
+
+rem if the global conf file is empty
 if "%e%" == "" ( 
     rem https://stackoverflow.com/questions/11153532/how-to-iterate-string-in-cmd-for-loop
-    for /f "tokens=1-4" %%a in ('%~dp0SelectDevice.exe') do (
+    for /f "tokens=1-4" %%a in ('%~dp0../bin/SelectDevice.exe %1') do (
         rem the user choosed a board from the list
+        rem this board will be saved as a project conf file
         set e=%%a
         set f=%%b
         set g=%%c
