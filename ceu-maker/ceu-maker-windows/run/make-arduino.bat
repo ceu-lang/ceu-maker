290d0f37688930a00ed9c91d9aaa790129b58bb0
AnnyCaroline <annycarolinegnr@gmail.com>
2019-03-04 16:00:02 -0300

    [windows] solve bug: error when compile a .ceu file with 'open with'

--- ceu-maker/ceu-maker-windows/run/make-arduino.bat
+++ ceu-maker/ceu-maker-windows/run/make-arduino.bat
@@ -1,10 +1,15 @@
 @echo off
 
-for %%i IN ("%1") do (set fileextension=%%~xi)
+set arg=%1
+set arg=%arg:"=%
+
+echo %arg%
+
+for %%i IN ("%arg%") do (set fileextension=%%~xi)
 
 rem if we're trying to compile a folder
 if "%fileextension%"=="" (
-    set boardPath=%1\board.conf
+    set boardPath=%arg%\board.conf
 ) else (
     set boardPath=board.conf
 )
@@ -32,7 +37,7 @@ if "%e%" == "" (
 rem if the global conf file is empty
 if "%e%" == "" ( 
     rem https://stackoverflow.com/questions/11153532/how-to-iterate-string-in-cmd-for-loop
-    for /f "tokens=1-4" %%a in ('%~dp0../bin/SelectDevice.exe %1') do (
+    for /f "tokens=1-4" %%a in ('%~dp0../bin/SelectDevice.exe %arg%') do (
         rem the user choosed a board from the list
         rem this board will be saved as a project conf file
         set e=%%a
@@ -46,6 +51,6 @@ if "%e%" == "" (
     echo No board selected. Select a board using SelectDevice or edit the board.conf file.
 ) else (
     cd %~dp0\..\repos\ceu-arduino\
-    ..\..\mingw\bin\make -f Makefile CEU_SRC=%1 ARD_BOARD=%e% ARD_PORT=%f% ARD_CPU=%g% ARD_MCU=%h%
+    ..\..\mingw\bin\make -f Makefile CEU_SRC=%arg% ARD_BOARD=%e% ARD_PORT=%f% ARD_CPU=%g% ARD_MCU=%h%
 )
 pause
